cbee3f8d57f601801f5d2215845ccd83f47dc66a
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-05-28 09:21:57 -0500

    Delete practiceexam.js

--- practiceexam.js
+++
@@ -1,9 +0,0 @@
-class Question {
-  constructor (prompy, correctAnswer) {
-    this.prompt = prompt;
-    this.correctAnswer = correctAnswer;
-  }
-  checkAnswer(userInput) {
-    return userInput.trim().toLowerCase() === this.correctAnswer.toLowerCase(); 
-  }
-}

c1bf7f003b60adb5caedb5389d88f62afcd28ee2
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-05-28 11:25:14 -0500

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,5 +1,37 @@
 // Creating a budget class using my python budget 
 //Translating the Python code to JavaScript code using W3Schools
+
+/* The Python Code: 
+class Budget:
+    def __init__ (self,name,amount,rent,lights,car,phone,discretionary,needs,laundry):
+        self.name = name
+        self.amount = amount
+        self.rent = rent
+        self.lights = lights
+        self.car = car
+        self.phone = phone
+        self.discretionary = discretionary
+        self.needs = needs
+        self.laundry = laundry
+    def monthly_budget(self):
+        return f'Hello {self.name}. Ready to Budget?'
+    def leftover(self):
+        if (self.amount - (self.rent+self.lights+self.car+self.phone+self.discretionary+self.needs+self.laundry)) >= 0:
+            print("Hello", self.name, "Your leftover balance for the month is: $", self.amount - (self.rent+self.lights+self.car+self.phone+self.discretionary+self.needs+self.laundry))
+        elif (self.amount - (self.rent+self.lights+self.car+self.phone+self.discretionary+self.needs+self.laundry))<0:
+            print("So..um..", self.name, 'You are screwed!!!')
+print('Hello, are you ready to begin?')
+print('')
+my_budget = Budget("Hannah",int(input("Your Monthly Amount is: $")),int(input("Your Rent is: $")),int(input("Your Lights are: $")),int(input("Your Car is: $")),int(input("Your Phone is: $")),int(input("Your Discretionary is: $")),int(input("Your Needs Are: $")),int(input("Your Laundry is: $")))
+my_budget.leftover()
+
+total = 0
+for num in [1, 2, 3, 4]:
+    total += num
+
+print(total) */
+
+// The JavaScript Code:
 class Budget { 
   
 }

99bfbeb66f0ad90ad5aebe597fe49d93befa1e25
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-06-02 17:10:01 -0500

    Create practice exam.js

---
+++ practice exam.js
@@ -0,0 +1,24 @@
+function CalcPersonality () {
+  var monit; // You monitor others and do not have a personality of your own 
+  var flalt; // False Altruist - Pretends to be altruistic to hide that you don't care 
+  var pet; // You are the pet to any authority figure 
+  var bomb; // You are hard to work with because you are a walking time bomb
+  var fake; // You are such a fake person, you even lie to yourself. You will never experience true love
+  var intr; //You pretend to be in introvert to hide your crippling social anxiety 
+  var extr; // You are the life of the party when no one asked you to be. Your presence is unneccessary 
+  var irr; // You can't cope with how irrelevant you are 
+  var cold; //You have no empathy for others and struggle with developing meaningful relationships
+  var perf; //You pretend to have it all together, but secretly want someone to save you. Grow up. 
+  var hero; //You try to help others so that they will owe you and never leave you
+  var lube; //I know your kinks are rare, but there's nothing to be ashamed of...
+  var brkmtl; //You will never escape the broke mentality 
+  var perf; //You will never achieve what you want in life because you already know that to be true
+  var bod; // Your body will always say yes when your mind says no 
+  var glass; // Unfortunately you are so transparent, others can see more of you than you can see yourself
+  var codep; // You will always be the side character, the support for someone else's dreams. You are only a concept defined by those you attach yourself to 
+  var holy; //There is no reason to pretend you are a person af virture
+  var dis; // Everything you touch turns into disaster. You are better off alone
+  var sad; //Your mother couldn't even love your face, you are such a disgrace
+  
+  
+}

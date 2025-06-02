9c7b90b92e7881552479b12464cf87b79b2a71dd
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-06-02 17:49:10 -0500

    Update practiceexam.js

--- practiceexam.js
+++ practiceexam.js
@@ -1,24 +1,24 @@
-function CalcPersonality () {
-  var monit; // You monitor others and do not have a personality of your own 
-  var flalt; // False Altruist - Pretends to be altruistic to hide that you don't care 
-  var pet; // You are the pet to any authority figure 
-  var bomb; // You are hard to work with because you are a walking time bomb
-  var fake; // You are such a fake person, you even lie to yourself. You will never experience true love
-  var intr; //You pretend to be in introvert to hide your crippling social anxiety 
-  var extr; // You are the life of the party when no one asked you to be. Your presence is unneccessary 
-  var irr; // You can't cope with how irrelevant you are 
-  var cold; //You have no empathy for others and struggle with developing meaningful relationships
+function BadTherapy () {
+  var monit; // Stop copying others and get a personality  
+  var flalt; // Stop pretending to be an altruist. We both know you don't care about these people  
+  var pet; // You lick the boots of anyone in authority 
+  var bomb; // Everyone walks on eggshells around you. Don't believe them when they say they don't. You're a ticking time bomb. 
+  var fake; // You lie to others, lie to yourself, and even your heart lies to you. You will try to chase, but love will always be one step away of you. 
+  var intr; // You aren't an introvert. You just say that when you don't like the person you're talking to. 
+  var extr; // You are the life of the party but no one invited you. Your presence is unneccessary 
+  var irr; // You can't cope with how irrelevant you are. You will never be good enough. Stop lying to yourself
+  var cold; // You secretly enjoy the pain of others. Stop pretending to be sad and just laugh in their face already.
   var perf; //You pretend to have it all together, but secretly want someone to save you. Grow up. 
-  var hero; //You try to help others so that they will owe you and never leave you
+  var hero; //You love to be the hero, but you would rather pretend to be the victim than accept when you're the villian. 
   var lube; //I know your kinks are rare, but there's nothing to be ashamed of...
   var brkmtl; //You will never escape the broke mentality 
-  var perf; //You will never achieve what you want in life because you already know that to be true
-  var bod; // Your body will always say yes when your mind says no 
-  var glass; // Unfortunately you are so transparent, others can see more of you than you can see yourself
-  var codep; // You will always be the side character, the support for someone else's dreams. You are only a concept defined by those you attach yourself to 
+  var pess; //You will never achieve what you want in life because you already know that to be true
+  var sad; // Happiness is chasing you, but you are fast
+  var glass; // Unfortunately you are so transparent, others can see more of you than you can see of yourself
+  var codep; // You don't enable other people's bad behavior because you're kind. You want to see them destroy themselves so you can save them. 
   var holy; //There is no reason to pretend you are a person af virture
-  var dis; // Everything you touch turns into disaster. You are better off alone
-  var sad; //Your mother couldn't even love your face, you are such a disgrace
+  var dis; // Everyone already knows you are an imposter. They are just waiting for you to admit you're incompetent, but we both know that you never will
+  var brkn; // You are a disappointment to anyone who tries to love you. Dist yourself off. You're the only one who can.
   
   
 }

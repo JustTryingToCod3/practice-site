ac43bd42cecc605f682f7c711237171d8ebd01ec
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-06-02 18:33:34 -0500

    Update badtherapy.js

--- badtherapy.js
+++ badtherapy.js
@@ -1,19 +1,26 @@
 function BadTherapy () {
+  //Your Personality
   var monit; // Stop copying others and get a personality  
   var flalt; // Stop pretending to be an altruist. We both know you don't care about these people  
   var pet; // You lick the boots of anyone in authority 
   var bomb; // Everyone walks on eggshells around you. Don't believe them when they say they don't. You're a ticking time bomb. 
-  var fake; // You lie to others, lie to yourself, and even your heart lies to you. You will try to chase, but love will always be one step away of you. 
+  var fake; // You lie to others, lie to yourself, and even your heart lies to you. You will try to chase, but love will always be one step away of you.
+
+  //Your Narrative 
   var intr; // You aren't an introvert. You just say that when you don't like the person you're talking to. 
   var extr; // You are the life of the party but no one invited you. Your presence is unneccessary 
   var irr; // You can't cope with how irrelevant you are. You will never be good enough. Stop lying to yourself
   var cold; // You secretly enjoy the pain of others. Stop pretending to be sad and just laugh in their face already.
   var perf; //You pretend to have it all together, but secretly want someone to save you. Grow up. 
+
+  //Your Thoughts and actions
   var hero; //You love to be the hero, but you would rather pretend to be the victim than accept when you're the villian. 
   var lube; //I know your kinks are rare, but there's nothing to be ashamed of...
   var brkmtl; //You will never escape the broke mentality 
   var pess; //You will never achieve what you want in life because you already know that to be true
   var sad; // Happiness is chasing you, but you are fast
+
+  // What you fail to admit 
   var glass; // Unfortunately you are so transparent, others can see more of you than you can see of yourself
   var codep; // You don't enable other people's bad behavior because you're kind. You want to see them destroy themselves so you can save them. 
   var holy; //There is no reason to pretend you are a person af virture

32bb175a16c4f6c96b6f5b32bb02d0be098a5c4f
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-06-02 19:36:44 -0500

    Update badtherapy.js

--- badtherapy.js
+++ badtherapy.js
@@ -1,31 +1,25 @@
 function BadTherapy () {
-  //Your Personality
-  var monit; // Stop copying others and get a personality  
-  var flalt; // Stop pretending to be an altruist. We both know you don't care about these people  
-  var pet; // You lick the boots of anyone in authority 
-  var bomb; // Everyone walks on eggshells around you. Don't believe them when they say they don't. You're a ticking time bomb. 
-  var fake; // You lie to others, lie to yourself, and even your heart lies to you. You will try to chase, but love will always be one step away of you.
-
-  //Your Narrative 
-  var intr; // You aren't an introvert. You just say that when you don't like the person you're talking to. 
-  var extr; // You are the life of the party but no one invited you. Your presence is unneccessary 
-  var irr; // You can't cope with how irrelevant you are. You will never be good enough. Stop lying to yourself
+  
+  var frus; // You will never reach an orgasm   
+  var brkn; // You are a disappointment to anyone who tries to love you. Dust yourself off. You're the only one who can.
   var cold; // You secretly enjoy the pain of others. Stop pretending to be sad and just laugh in their face already.
+  var codep; // You don't enable other people's bad behavior because you're kind. You want to see them destroy themselves so you can save them.
+  var hero; //You love to be the hero, but you would rather pretend to be the victim than accept when you're the villian.
   var perf; //You pretend to have it all together, but secretly want someone to save you. Grow up. 
-
-  //Your Thoughts and actions
-  var hero; //You love to be the hero, but you would rather pretend to be the victim than accept when you're the villian. 
+  var flalt; // Stop pretending to be an altruist. We both know you don't care about these people  
+  var holy; //There is no reason to pretend you are a person af virture. You are not.
   var lube; //I know your kinks are rare, but there's nothing to be ashamed of...
+  var irr; // You can't cope with how irrelevant you are. You are only good enough to be a disappointment
   var brkmtl; //You will never escape the broke mentality 
-  var pess; //You will never achieve what you want in life because you already know that to be true
   var sad; // Happiness is chasing you, but you are fast
-
-  // What you fail to admit 
-  var glass; // Unfortunately you are so transparent, others can see more of you than you can see of yourself
-  var codep; // You don't enable other people's bad behavior because you're kind. You want to see them destroy themselves so you can save them. 
-  var holy; //There is no reason to pretend you are a person af virture
+  var intr; // You aren't an introvert. You just say that when you don't like the person you're talking to. 
+  var extr; // No one truly likes talking to you. They just talk to you because they feel bad for you.  
+  var pess; //You will never achieve what you want in life and you already know that
   var dis; // Everyone already knows you are an imposter. They are just waiting for you to admit you're incompetent, but we both know that you never will
-  var brkn; // You are a disappointment to anyone who tries to love you. Dist yourself off. You're the only one who can.
+  var fake; // You lie to others, lie to yourself, and even your heart lies to you. You will try to chase, but love will always be one step away of you.
+  var bomb; // Everyone walks on eggshells around you. Don't believe them when they say they don't. You're a ticking time bomb. 
+  var glass; // Unfortunately you are so transparent, others can see more of you than you can see of yourself
+  var monit; // Stop fucking around and get a personality 
 
   
   

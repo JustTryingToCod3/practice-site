f7bce2f2ae2418f3e082642de25ba58ff32a79fa
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-09-06 12:04:56 -0500

    Update gen.js

--- gen.js
+++ gen.js
@@ -1,1 +1,10 @@
+const tarotMain = document.getElementById("tarotMain");
+const genReadingBtn = document.getElementById("genreading");
+const ppfBtn = document.getElementById("ppf");
+const mainP = document.getElementById("mainp");
+const supP = document.getElementById("supP");
+const present = document.getElementById("present");
+const future = document.getElementById("future");
+const encourageBtn = document.getElementById("encourage");
+const encouragement = document.getElementById("encouragement");
 

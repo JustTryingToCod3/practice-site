685fb9876419f5ada18b55d106b7f8504e359440
Just_Here <171275367+JustTryingToCod3@users.noreply.github.com>
2025-09-10 13:56:01 -0500

    Update gen.js

--- gen.js
+++ gen.js
@@ -1,41 +1,42 @@
-Class GenRead extends Deck {
-  genRead{
+class GenRead extends Deck {
+  constructor() {
     super();
-    this.headM = Math.floor(Math.random()*Deck.headerMessages().length);
-    this.supM = Math.floor(Math.random()*Deck.supportingMessages().length);
-    this.Past = Math.floor(Math.random()*Deck.past().length);
-    this.present = Math.floor(Math.random()*Deck.present().length);
-    this.future = Math.floor(Math.random()*Deck.future().length);
-    this.encourage = Math.floor(Math.random()*Deck.encourage().length);
+    this.headP = Math.floor(Math.random() * Deck.headerMessages().length);
+    this.supP = Math.floor(Math.random() * Deck.supportingMessages().length);
+    this.pastP = Math.floor(Math.random() * Deck.past().length);
+    this.presentP = Math.floor(Math.random() * Deck.present().length);
+    this.futureP = Math.floor(Math.random() * Deck.future().length);
+    this.encourageP = Math.floor(Math.random() * Deck.encourage().length);
     
     //HTML BUTTONS 
     this.genReadingBtn = document.getElementById("genreading"); //general reading button
     this.ppfBtn = document.getElementById("ppf"); //Past present future button 
     this.mainP = document.getElementById("mainp"); //main paragraph
     this.supP = document.getElementById("supP"); // supporting paragraph and past paragraph
-    this.present = document.getElementById("present"); //present paragrapg
+    this.present = document.getElementById("present"); //present paragraph
     this.future = document.getElementById("future"); //future paragraph
     this.encourageBtn = document.getElementById("encourage"); //encouragement button 
     this.encouragement = document.getElementById("encouragement"); //encouragement paragraph 
   }
-  general(){
-      document.getElementById("demo").innerHTML = this.headM;
-    
+
+  general() {
+    document.getElementById("mainp").innerHTML = this.headP;
+    document.getElementById("supP").innerHTML = this.supP;
   }
-  ppf(){
-    
+
+  ppf() {
+    document.getElementById("supP").innerHTML = this.pastP;
+    document.getElementById("present").innerHTML = this.presentP;
+    document.getElementById("future").innerHTML = this.futureP;
   }
-  encourage(){
-    
+
+  encourage() {
+    document.getElementById("encouragement").innerHTML = this.encourageP;
   }
-  
 }
 
 let getRead = new GenRead();
 
-
-genReadingBtn.addEventListener("click", getRead.general(){});
-ppfBtn.addEventListener("click", getRead.ppf(){});
-encourageBtn.addEventListener("click", getRead.encourage(){});
-
-
+getRead.genReadingBtn.addEventListener("click", () => getRead.general());
+getRead.ppfBtn.addEventListener("click", () => getRead.ppf());
+getRead.encourageBtn.addEventListener("click", () => getRead.encourage());
